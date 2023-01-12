53c419fb2daafff40f69ab888e139e2dd24b7a63
thijsBoet <m.boet@chello.nl>
2023-01-12 18:19:05 +0100

    created popover and tests for set popover

--- sundaes-on-demand/src/components/pages/summary/SummaryForm.js
+++ sundaes-on-demand/src/components/pages/summary/SummaryForm.js
@@ -1,31 +1,43 @@
 import React, { useState } from 'react';
 import { Form, Button} from 'react-bootstrap';
-
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Popover from 'react-bootstrap/Popover';
 
 
 const SummaryForm = () => {
 	const [isDisabled, setIsDisabled] = useState(true);
 
-	const checkboxLabel = (
-		<label htmlFor='terms'>
-			I agree to <a href=''>Terms and Conditions</a>
-		</label>
+	const popover = (
+		<Popover id='popover-basic'>
+			<Popover.Body>
+				No ice cream will actually be delivered
+			</Popover.Body>
+		</Popover>
 	);
+
+	const checkboxLabel = (
+		<span>
+			I agree to
+			<OverlayTrigger placement='right' overlay={popover}>
+				<a href=''> Terms and Conditions</a>
+			</OverlayTrigger>
+		</span>
+	);
+
 
 	return (
 		<Form>
 			<Form.Group controlId='terms-and-conditions'>
-			<Form.Check
-				type='checkbox'
-				name='terms'
-				id='terms'
-				defaultChecked={false}
-				value={isDisabled}
-				onClick={() => setIsDisabled(!isDisabled)}
+				<Form.Check
+					type='checkbox'
+					name='terms'
+					id='terms'
+					defaultChecked={false}
+					value={isDisabled}
+					onClick={() => setIsDisabled(!isDisabled)}
+					label={checkboxLabel}
 				/>
 			</Form.Group>
-			{checkboxLabel}
-			<br />
 			<Button name='submit' type='submit' disabled={isDisabled}>
 				Confirm order
 			</Button>
--- sundaes-on-demand/src/components/pages/summary/test/SummaryForm.test.js
+++ sundaes-on-demand/src/components/pages/summary/test/SummaryForm.test.js
@@ -1,35 +1,70 @@
 import { render, screen } from '@testing-library/react';
 import { describe, it, test } from '@jest/globals';
+import userEvent from '@testing-library/user-event';
 import SummaryForm from '../SummaryForm';
 
-describe('Checkbox', () => {
+describe('Summary Form Component', () => {
 	test('button should be disabled by default', () => {
 		render(<SummaryForm />);
+
 		const button = screen.getByRole('button', { name: /confirm order/i });
+
 		expect(button).toBeDisabled();
 	});
 	it('should be unchecked by default', () => {
 		render(<SummaryForm />);
-		const checkbox = screen.getByRole('checkbox', { name: /terms and conditions/i });
+
+		const checkbox = screen.getByRole('checkbox', {
+			name: /terms and conditions/i,
+		});
+
 		expect(checkbox).not.toBeChecked();
 	});
 
-	it('should enable button by checking checkbox', () => {
+	it('should enable button by checking checkbox', async () => {
+		const user = userEvent.setup();
 		render(<SummaryForm />);
 		const checkbox = screen.getByRole('checkbox');
-		const button = screen.getByRole('button', {name: /confirm order/i});
+		const button = screen.getByRole('button', { name: /confirm order/i });
+
 		expect(button).toBeDisabled();
-		checkbox.click();
+
+		await user.click(checkbox);
 		expect(button).toBeEnabled();
 	});
 
-	it('should disable button by unchecking checkbox', () => {
+	it('should disable button by unchecking checkbox', async () => {
+		const user = userEvent.setup();
 		render(<SummaryForm />);
 		const checkbox = screen.getByRole('checkbox');
-		const button = screen.getByRole('button', {name: /confirm order/i});
-		checkbox.click();
+		const button = screen.getByRole('button', { name: /confirm order/i });
+
+		await user.click(checkbox);
 		expect(button).toBeEnabled();
-		checkbox.click();
+
+		await user.click(checkbox);
 		expect(button).toBeDisabled();
 	});
+
+	test('popover text should not be in document without hover', async () => {
+		render(<SummaryForm />);
+		const nullPopover = screen.queryByText(/no ice cream will actually be delivered/i);
+
+		expect(nullPopover).not.toBeInTheDocument();
+	});
+
+	test('popover text should be in document on hover', async () => {
+		const user = userEvent.setup();
+		render(<SummaryForm />);
+		const termsAndConditions = screen.getByText(/terms and conditions/i);
+
+		await user.hover(termsAndConditions);
+		const popover = screen.getByText(/no ice cream will actually be delivered/i);
+
+		expect(popover).toBeInTheDocument();
+
+		await user.unhover(termsAndConditions);
+		expect(popover).not.toBeInTheDocument();
+	});
+
 });
